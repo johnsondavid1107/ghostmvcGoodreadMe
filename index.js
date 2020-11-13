a2de11436344edf8fbd4dac6ee179394a53e1253
Ness <johnsondavid1107@gmail.com>
2020-11-13 14:50:49 -0500

    testing the license feature

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const writeFile = (replies) => 
 
-    `This project was created by ${replies.name}.  
+const writeFile = (replies) =>
+    `
+    Table of Contents
+    Installation
+    ${replies.install}
+
+    Usage
+    ${replies.usage}
+    License
+    ${replies.license}
+    Contributions
+    ${replies.contribution}
+    Tests
+    ${replies.test}
+    Questions
+    ${replies.question}
+
+
+    
+    
+    
+    
+    
+    
+    This project was created by ${replies.name}.  
     They can be contacted by email at ${replies.eMail}.
-    The GitHub Account information is ${replies.gitHub}.`
-
-
-
-;
-
+    The GitHub Account information is ${replies.gitHub}.
+    Here is a description of this application ${replies.description}.
+    You like these foods! ${replies.food}
+    
+    
+    
+    
+    
+    
+    
+    
+    `;
 
 async function askReadMe() {
 
@@ -19,36 +48,64 @@ async function askReadMe() {
 
             {
                 type: "confirm",
-                message: "Hello, I am Jarvis, your personal automated assistant.\n  I will be assiting you with creating an automated readme .md file, \n however I will need to ask you a few questions before we begin.\n  Do you accept?" ,
+                message: "Hello, I am Jarvis, your personal automated assistant.\n  I will be assiting you with creating an automated readme .md file \n however, I will need to ask you a few questions before we begin.\n  Do you accept?",
                 name: "acceptance"
             },
             {
                 type: "input",
-                message: "First, what is the name of the project" ,
+                message: "First, what is the name of the project?",
                 name: "name"
             },
             {
                 type: "input",
-                message: "Let's have an email address" ,
+                message: "Let's have an email address..",
                 name: "eMail"
             },
             {
                 type: "input",
                 message: "How about a GitHub Account name",
                 name: "gitHub"
+    
+            },
+            {
+                type: "input",
+                message: "Please enter a description",
+                name: "description"
+            },
+            {
+                type: "list",
+                message: "Which one of these do you like best?",
+                name: "food",
+                choices:["Pizza", "Hamburgers", "Fries"] 
+            },
+            {
+                type: "input",
+                message: "How does one install the app?",
+                name: "install"
+            },
+            {
+                type: "input",
+                message: "How can this application be used?",
+                name: "usage"
+            },
+            {
+                type: "input",
+                message: "Who currently contributes, and how would you like contributions to be done?",
+                name: "contribution"
             },
 
 
         ])
+        console.log(askMe);
 
-const write = writeFile(replies);
+        const write = writeFile(askMe);
 
         fs.writeFileSync(`${askMe.name}.md`, write);
 
         console.log(".md is good");
-    } catch(err) {
+    } catch (err) {
         console.error(err);
-     }
+    }
 
 
 
